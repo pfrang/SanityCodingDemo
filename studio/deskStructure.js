c79f4d198fb6673aa028d4531dd00200e4f9b2cd
Peder Frang <peder.frang@gmail.com>
2022-03-14 10:04:10 +0100

    first

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -41,13 +41,26 @@ export default () =>
         .schemaType("post")
         .child(S.documentTypeList("post").title("Posts"))
         ,
-       
+
       S.listItem()
         .title("Authors")
         .icon(MdAccountCircle)
         .schemaType("author")
         .child(S.documentTypeList("author").title("Authors")),
-     
-
+      S.listItem()
+        .title("Posts by author")
+        .icon(MdGroup)
+        .child(
+          S.documentTypeList("author")
+            .title("Posts by author")
+            .child(
+              (_id) =>
+                S.documentList("post")
+                  .schemaType("post")
+                  .title("Posts by author")
+                  .filter('_type == "post" && author._ref == $_id')
+                  .params({ _id })
+            )
+        ),
       ...S.documentTypeListItems().filter(hiddenDocTypes),
     ]);
